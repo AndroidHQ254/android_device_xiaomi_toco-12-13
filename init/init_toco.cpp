96d26e9a1cf69f20ab85c163e2dc454b944cda1c
AndroidHQ254 <d.murimi14@gmail.com>
2021-04-22 20:59:52 +0300

    toco: Rework init
    * Adapt from @chemusqui1 tucana tree
    * Source: https://github.com/chemusqui1/device_xiaomi_tucana-testing/tree/master/init

--- init/init_toco.cpp
+++ init/init_toco.cpp
@@ -1,7 +1,7 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2019-2020 The LineageOS Project.
+   Copyright (c) 2014, The Linux Foundation. All rights reserved.
+   Copyright (C) 2021 The LineageOS Project.
+
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -14,6 +14,7 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
+
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -27,116 +28,114 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <fstream>
-#include <unistd.h>
 #include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "property_service.h"
-#include "vendor_init.h"
+#include <sys/sysinfo.h>
 
 using android::base::GetProperty;
 
-int property_set(const char *key, const char *value) {
-    return __system_property_set(key, value);
-}
-
-constexpr const char *RO_PROP_SOURCES[] = {
-    nullptr,   "product.", "product_services.", "odm.",
-    "vendor.", "system.", "system_ext.", "bootimage.",
-};
-
-constexpr const char *BRANDS[] = {
-    "Xiaomi",
-    "Xiaomi",
-};
-
-constexpr const char *PRODUCTS[] = {
-    "tucana",
-    "toco",
-};
-
-constexpr const char *DEVICES[] = {
-    "Mi Note 10",
-    "Mi Note 10 Lite",
-};
-
-constexpr const char *BUILD_DESCRIPTION[] = {
-    "redfin-user 11 RQ2A.210405.005/7181113 release-keys",
-    "redfin-user 11 RQ2A.210405.005/7181113 release-keys",
-};
-
-constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys",
-    "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys",
-};
-
-constexpr const char *CLIENT_ID[] = {
-    "android-xiaomi",
-    "android-xiaomi",
-};
-
 void property_override(char const prop[], char const value[], bool add = true) {
-  prop_info *pi;
+    prop_info* pi;
 
-  pi = (prop_info *)__system_property_find(prop);
-  if (pi)
-    __system_property_update(pi, value, strlen(value));
-  else if (add)
-    __system_property_add(prop, strlen(prop), value, strlen(value));
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void load_props(const char *model, bool is_in = false) {
-  const auto ro_prop_override = [](const char *source, const char *prop,
-                                   const char *value, bool product) {
-    std::string prop_name = "ro.";
+void load_dalvikvm_properties() {
+    struct sysinfo sys;
 
-    if (product)
-      prop_name += "product.";
-    if (source != nullptr)
-      prop_name += source;
-    if (!product)
-      prop_name += "build.";
-    prop_name += prop;
-
-    property_override(prop_name.c_str(), value);
-  };
-
-  for (const auto &source : RO_PROP_SOURCES) {
-    ro_prop_override(source, "device", is_in ? PRODUCTS[1] : PRODUCTS[0], true);
-    ro_prop_override(source, "model", model, true);
-    if (!is_in) {
-      ro_prop_override(source, "brand", BRANDS[0], true);
-      ro_prop_override(source, "name", PRODUCTS[0], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[0], false);
+    sysinfo(&sys);
+    if (sys.totalram < 6144ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heapmaxfree", "32m");
     } else {
-      ro_prop_override(source, "brand", BRANDS[1], true);
-      ro_prop_override(source, "name", PRODUCTS[1], true);
-      ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[1], false);
+        // 8GB & 12GB RAM
+        property_override("dalvik.vm.heapstartsize", "32m");
+        property_override("dalvik.vm.heapgrowthlimit", "512m");
+        property_override("dalvik.vm.heapsize", "768m");
+        property_override("dalvik.vm.heapmaxfree", "64m");
     }
-  }
 
-  if (!is_in) {
-    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[0], false);
-    property_override("ro.boot.product.hardware.sku", PRODUCTS[0]);
-  } else {
-    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[1], false);
-    property_override("ro.com.google.clientidbase", CLIENT_ID[0]);
-    property_override("ro.com.google.clientidbase.ms", CLIENT_ID[1]);
-  }
-  ro_prop_override(nullptr, "product", model, false);
+    property_override("dalvik.vm.heaptargetutilization", "0.5");
+    property_override("dalvik.vm.heapminfree", "8m");
 }
+
+std::vector<std::string> ro_props_default_source_order = {
+        "", "bootimage.", "odm.", "product.", "system.", "system_ext.", "vendor.",
+};
+
+void set_ro_build_prop(const std::string& prop, const std::string& value) {
+    for (const auto& source : ro_props_default_source_order) {
+        auto prop_name = "ro." + source + "build." + prop;
+        if (source == "")
+            property_override(prop_name.c_str(), value.c_str());
+        else
+            property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
+
+void set_ro_product_prop(const std::string& prop, const std::string& value) {
+    for (const auto& source : ro_props_default_source_order) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
 
 void vendor_load_properties() {
-  std::string region;
-  region = GetProperty("ro.boot.hwc", "");
+    std::string region;
+    std::string hardware_revision;
+    region = GetProperty("ro.boot.hwc", "GLOBAL");
+    hardware_revision = GetProperty("ro.boot.hwversion", "UNKNOWN");
 
-  if (region == "CN") {
-    load_props(DEVICES[0], false);
-  } else if (region == "GLOBAL") {
-    load_props(DEVICES[1], true);
-  }
+    std::string model;
+    std::string device;
+    std::string fingerprint;
+    std::string description;
+    std::string mod_device;
+
+    if (region == "GLOBAL") {
+        model = "Mi Note 10 Lite";
+        device = "toco";
+        fingerprint =
+                "Xiaomi/toco_global/toco:11/RKQ1.200826.002/V12.1.4.0.RFNMIXM:user/release-keys";
+        description = "toco-user 10 RKQ1.200826.002 V12.1.4.0.RFNMIXM release-keys";
+        mod_device = "toco_global";
+    } else if (region == "RU") {
+        model = "Mi Note 10 Lite";
+        device = "toco";
+        fingerprint =
+                "Xiaomi/toco_global/toco:11/RKQ1.200826.002/V12.1.4.0.RFNMIXM:user/release-keys";
+        description = "toco-user 10 RKQ1.200826.002 V12.1.4.0.RFNMIXM release-keys";
+    } else if (region == "EEA") {
+        model = "Mi Note 10 Lite";
+        device = "toco";
+        fingerprint =
+                "Xiaomi/toco_global/toco:11/RKQ1.200826.002/V12.1.4.0.RFNMIXM:user/release-keys";
+        description = "toco-user 10 RKQ1.200826.002 V12.1.4.0.RFNMIXM release-keys";
+        mod_device = "toco_global";
+    }
+
+    set_ro_build_prop("fingerprint", fingerprint);
+    set_ro_product_prop("device", device);
+    set_ro_product_prop("model", model);
+    property_override("ro.build.description", description.c_str());
+    if (mod_device != "") {
+        property_override("ro.product.mod_device", mod_device.c_str());
+    }
+
+    property_override("ro.boot.hardware.revision", hardware_revision.c_str());
+    property_override("ro.apex.updatable", "true");
+    property_override("ro.oem_unlock_supported", "0");
+
+    load_dalvikvm_properties();
 }
