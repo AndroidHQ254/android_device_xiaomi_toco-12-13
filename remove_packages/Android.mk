1876bcc252861ac609e051070d2d2bada33e686e
AndroidHQ254 <d.murimi14@gmail.com>
2021-11-10 22:40:10 +0300

    toco: Remove more packages at build time
    Squash;
    - Rename RemovePackages for source
    - Switch to platform certificate
    
    Co-Authored-By: Nathan <35746622+Lunarixus@users.noreply.github.com>
    Co-Authored-By: Naveen Johnson <14140949+naveenjohnsonv@users.noreply.github.com>
    Co-Authored-By: Janicki Artur <28084+Arturo@users.noreply.github.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -1,17 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := RemovePackages
+LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService Maps YouTube Drive PrebuiltGmail
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Showcase
-LOCAL_OVERRIDES_PACKAGES += SprintDM SprintHM USCCDM
-LOCAL_OVERRIDES_PACKAGES += VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Drive ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt RecorderPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
