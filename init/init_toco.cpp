5658185fa1fb980bc92727f22546eeca315f84c2
AndroidHQ254 <d.murimi14@gmail.com>
2021-12-14 21:51:32 +0300

    toco: init: Rework init
    Co-Authored-By: luk1337 <priv.luk@gmail.com>
    Co-Authored-By: Abhishek Aggarwal <13186733+TheScarastic@users.noreply.github.com>

--- init/init_toco.cpp
+++ init/init_toco.cpp
@@ -1,69 +1,49 @@
 /*
-   Copyright (c) 2014, The Linux Foundation. All rights reserved.
-   Copyright (C) 2021 The LineageOS Project.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2021, The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <cstdlib>
+#include <fstream>
+#include <string.h>
+#include <unistd.h>
 #include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-
 #include <sys/sysinfo.h>
 
-using android::base::GetProperty;
+#include "property_service.h"
+#include "vendor_init.h"
 
 void property_override(char const prop[], char const value[], bool add = true) {
-    prop_info* pi;
+    prop_info *pi;
 
-    pi = (prop_info*) __system_property_find(prop);
+    pi = (prop_info *)__system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void load_dalvikvm_properties() {
+void load_dalvik_properties() {
     struct sysinfo sys;
 
     sysinfo(&sys);
-    if (sys.totalram < 6144ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        property_override("dalvik.vm.heapstartsize", "16m");
-        property_override("dalvik.vm.heapgrowthlimit", "256m");
-        property_override("dalvik.vm.heapsize", "512m");
-        property_override("dalvik.vm.heapmaxfree", "32m");
-    } else {
+    if (sys.totalram > 7168ull * 1024 * 1024) {
         // 8GB & 12GB RAM
         property_override("dalvik.vm.heapstartsize", "32m");
         property_override("dalvik.vm.heapgrowthlimit", "512m");
         property_override("dalvik.vm.heapsize", "768m");
         property_override("dalvik.vm.heapmaxfree", "64m");
+    } else {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heapmaxfree", "32m");
     }
 
     property_override("dalvik.vm.heaptargetutilization", "0.5");
@@ -92,10 +72,8 @@ void set_ro_product_prop(const std::string& prop, const std::string& value) {
 };
 
 void vendor_load_properties() {
-    std::string region;
-    std::string hardware_revision;
-    region = GetProperty("ro.boot.hwc", "GLOBAL");
-    hardware_revision = GetProperty("ro.boot.hwversion", "UNKNOWN");
+    std::string region = android::base::GetProperty("ro.boot.hwc", "GLOBAL");
+    std::string hardware_revision = android::base::GetProperty("ro.boot.hwversion", "UNKNOWN");
 
     std::string model;
     std::string device;
@@ -134,8 +112,6 @@ void vendor_load_properties() {
     }
 
     property_override("ro.boot.hardware.revision", hardware_revision.c_str());
-    property_override("ro.apex.updatable", "true");
-    property_override("ro.oem_unlock_supported", "0");
 
-    load_dalvikvm_properties();
+    load_dalvik_properties();
 }
