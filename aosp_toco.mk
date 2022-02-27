184c373f93ed39d90503300fbf9047faee621d14
jhenrique09 <jhsv09@gmail.com>
2022-02-27 14:25:14 +0300

    toco: Enable quick tap support

--- aosp_toco.mk
+++ aosp_toco.mk
@@ -20,6 +20,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_toco
