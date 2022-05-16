081793cecb2296393febfe051de4c0a614aa7666
AndroidHQ254 <d.murimi14@gmail.com>
2022-05-16 14:18:12 +0300

    toco: Commonize media_profiles_vendor xml
    This reverts commit 3e588603146208ea18747117e861dfdef35c1455.

--- device.mk
+++ device.mk
@@ -85,10 +85,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
-
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
