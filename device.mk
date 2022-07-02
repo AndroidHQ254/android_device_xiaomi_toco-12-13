8fad8ceec253ffbfa8a05f79f2ad0b80d33a5542
Andrzej Perczak <linux@andrzejperczak.com>
2022-07-02 21:13:10 +0300

    toco: Import lmkd props from google gki
    Reference: https://github.com/LineageOS/android_hardware_google_pixel/blob/lineage-19.1/mm/device_gki.mk

--- device.mk
+++ device.mk
@@ -83,6 +83,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# LMK tuning
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.filecache_min_kb=153600 \
+    ro.lmk.stall_limit_critical=40
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
