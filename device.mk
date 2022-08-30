7f4b818c67ed344638b511fd416e4f663d5298c1
AndroidHQ254 <d.murimi14@gmail.com>
2022-08-30 14:09:37 +0300

    toco: Drop firmware inclusion by default
    Reverts commit c92b53f7fe7426185f33295142a1fc3ec2597c45

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 # MiuiCamera
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-# Firmware Toco
-$(call inherit-product, vendor/xiaomi-firmware/toco/firmware.mk)
-
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 
@@ -153,8 +150,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    vendor/xiaomi-firmware
+    $(LOCAL_PATH)
 
 # Storage health HAL
 PRODUCT_PACKAGES += \
