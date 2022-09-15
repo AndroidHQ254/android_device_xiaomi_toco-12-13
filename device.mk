9995ddd2767622920af92d1a7ac1dc03a8719209
AndroidHQ254 <d.murimi14@gmail.com>
2022-09-15 13:02:49 +0300

    toco: Drop keylayout from stock
    This reverts commit e764b4fce2e809ad952b723aaddc34f82b30f644.

--- device.mk
+++ device.mk
@@ -73,7 +73,6 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/configs/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl
 
 # IR
