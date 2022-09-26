257e5093a6a8741beeaaa5e7b135290c095afcc4
AndroidHQ254 <d.murimi14@gmail.com>
2022-09-26 14:30:56 +0300

    toco: Remove some packages at build time
    Partially reverts commit 1188e5599c3fa1252fea1c617bda60570b3ec117

---
+++ remove_packages/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePkgs
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := Drive Maps YouTube AppDirectedSMSService
+LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO DCMO DMService SprintDM
+LOCAL_OVERRIDES_PACKAGES += MyVerizonServices OBDM_Permissions Showcase
+LOCAL_OVERRIDES_PACKAGES += SprintHM USCCDM VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
