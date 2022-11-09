753d4dd4718c30e43131e40247331534b7b7b90e
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-11-09 16:49:50 +0300

    toco: Move sm6150-idp-snd-card_Button_Jack.kl to correct path

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_COPY_FILES += \
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/configs/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl
+    $(LOCAL_PATH)/configs/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl
 
 # IR
 PRODUCT_PACKAGES += \
