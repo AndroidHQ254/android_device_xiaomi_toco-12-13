82cdd159652a1626374b669f38ce4982aff22985
spkal01 <kalligeross@gmail.com>
2022-11-27 14:02:57 +0300

    toco: libinit: Add support of .mod_device hides (1/2)
    * Required hide for Miui Apps

--- libinit/init_xiaomi_toco.cpp
+++ libinit/init_xiaomi_toco.cpp
@@ -13,6 +13,7 @@ static const variant_info_t toco_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
+    .mod_device = "toco_global",
     .brand = "Xiaomi",
     .device = "toco",
     .marketname = "Xiaomi Mi Note 10 Lite",
@@ -25,6 +26,7 @@ static const variant_info_t toco_eea_info = {
     .hwc_value = "EEA",
     .sku_value = "",
 
+    .mod_device = "toco_global",
     .brand = "Xiaomi",
     .device = "toco",
     .marketname = "Xiaomi Mi Note 10 Lite",
@@ -37,6 +39,7 @@ static const variant_info_t toco_ru_info = {
     .hwc_value = "RU",
     .sku_value = "",
 
+    .mod_device = "toco_global",
     .brand = "Xiaomi",
     .device = "toco",
     .marketname = "Xiaomi Mi Note 10 Lite",
