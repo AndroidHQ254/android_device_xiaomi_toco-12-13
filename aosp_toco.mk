9b1f6c9da72cf86f7683e30903af747a499f69dd
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-12-02 23:47:42 +0300

    toco: Don't ship Google Recorder

--- aosp_toco.mk
+++ aosp_toco.mk
@@ -24,6 +24,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_toco
