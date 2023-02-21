e0ec82436766df33298cf3ee88ad25a830ae8cf3
AndroidHQ254 <d.murimi14@gmail.com>
2023-02-21 18:36:35 +0300

    toco: Do not include stock arcore

--- aosp_toco.mk
+++ aosp_toco.mk
@@ -25,6 +25,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ARCORE := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_toco
