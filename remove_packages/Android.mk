fa9291b332a1db74645fa274e61221561631e2c3
AndroidHQ254 <d.murimi14@gmail.com>
2023-04-06 21:42:13 +0300

    toco: Switch to pico gapps
    * Update remove_packages list while at it

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,21 +5,40 @@ LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AdaptiveVPNPrebuilt \
     arcore \
     AmbientStreaming \
+    CalculatorGooglePrebuilt \
+    Calendar \
+    Calendar2 \
+    CalendarGooglePrebuilt \
     CarrierLocation \
     CarrierMetrics \
     DevicePolicyPrebuilt \
     DiagnosticsToolPrebuilt \
+    ExactCalculator \
     Gallery2 \
+    GoogleTTS \
+    HealthConnectPrebuilt \
+    MaestroPrebuilt \
+    NgaResources \
     Papers \
     Photos \
     PhotoTable \
+    PicoTts \
     PixelLiveWallpaperPrebuilt \
+    PixelWallpapers2020 \
+    PixelWallpapers2021 \
     PixelWallpapers2022 \
+    PixelWallpapers2022a \
+    PixelWallpapers2023Tablet \
     RecorderPrebuilt \
+    ScribePrebuilt \
+    SecurityHubPrebuilt \
     Showcase \
-    SoundAmplifierPrebuilt
+    SoundAmplifierPrebuilt \
+    Tycho \
+    WellbeingPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
