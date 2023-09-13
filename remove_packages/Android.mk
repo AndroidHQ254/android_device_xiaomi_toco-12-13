89c9060348415a7cd9be81e0aa223d997dacc769
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2023-09-13 12:35:04 +0300

    toco: remove_packages: Refactor removed packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,10 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Drive Maps YouTube AppDirectedSMSService
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO DCMO DMService SprintDM
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices OBDM_Permissions Showcase
-LOCAL_OVERRIDES_PACKAGES += SprintHM Symphonica USCCDM VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_OVERRIDES_PACKAGES := Drive Maps YouTube
+LOCAL_OVERRIDES_PACKAGES += Showcase Symphonica
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
